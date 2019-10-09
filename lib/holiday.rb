bb1e384dbc4f90c6e65a51bee5df0f0db6763d1b
Jazmin M <jazminmerckel@gmail.com>
2019-10-09 23:50:04 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,7 +58,8 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-  contacts.each do |person, contact_details_hash|
+  holiday_hash.each do |season, holiday|
+    
 end
 
 def all_holidays_with_bbq(holiday_hash)
