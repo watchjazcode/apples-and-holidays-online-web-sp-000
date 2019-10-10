11a897aa9f733a9a7ccb4138b1a2e99fa2f067a2
Jazmin M <jazminmerckel@gmail.com>
2019-10-10 00:26:47 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -74,10 +74,16 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+    bbq_holidays = []
+    holiday_hash.each do |season, holiday|
+      holiday.each do |holiday_name, supplies|
+        if supplies.include?("BBQ")
+          bbq_holidays <<
+      end
+    end
+    return bbq_holidays
+  end
 end
-
-
 
 
 
